013fd66bf58632801c8d42b9f5cc67dc42048f57
Rohit <rohitkumar377024@gmail.com>
2020-08-01 10:45:42 +0530

    Added Basic Express Structure

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Furniture Store Backend');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Root Route'));
+
+app.listen(3000, () => console.log('Listening on Port 3000.'));
